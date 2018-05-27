a4bc60fdb90b3146489a4d6aaf9a2829cf4ee44f
Shamane Siriwardana <shamane92siriwardhana@gmail.com>
2018-05-28 11:42:13 +1200

    Add files via upload

--- network_models/discriminator.py
+++ network_models/discriminator.py
@@ -5,44 +5,66 @@ class Discriminator:
     def __init__(self, env):
         """
         :param env:
-        Output of this Discriminator is reward for learning agent. Not the cost.
+        Output of this Discriminator is reward for learning agent. Not the cost.  #prety important to understand 
         Because discriminator predicts  P(expert|s,a) = 1 - P(agent|s,a).
         """
 
         with tf.variable_scope('discriminator'):
             self.scope = tf.get_variable_scope().name
-            self.expert_s = tf.placeholder(dtype=tf.float32, shape=[None] + list(env.observation_space.shape))
-            self.expert_a = tf.placeholder(dtype=tf.int32, shape=[None])
-            expert_a_one_hot = tf.one_hot(self.expert_a, depth=env.action_space.n)
-            # add noise for stabilise training
-            expert_a_one_hot += tf.random_normal(tf.shape(expert_a_one_hot), mean=0.2, stddev=0.1, dtype=tf.float32)/1.2
-            expert_s_a = tf.concat([self.expert_s, expert_a_one_hot], axis=1)
+            self.expert_s = tf.placeholder(dtype=tf.float32, shape=[None] + list(env.observation_space.shape)) #to feed the exper's state
+            self.expert_a = tf.placeholder(dtype=tf.int32, shape=[None])  #to feed the expert actions inside the session
+            expert_a_one_hot = tf.one_hot(self.expert_a, depth=env.action_space.n)  #expert action as one hot action
 
-            self.agent_s = tf.placeholder(dtype=tf.float32, shape=[None] + list(env.observation_space.shape))
-            self.agent_a = tf.placeholder(dtype=tf.int32, shape=[None])
+            '''#If we want to use a LSTM to 
+            self.expert_s = tf.placeholder(tf.float32, [None, max_length, state_dim])
+            self.expert_a = tf.placeholder(tf.float32, [None, max_length, action])
+            expert_a_one_hot = tf.one_hot(self.expert_a, depth=number of actions)
+            '''
+
+            # add noise for stabilise training
+            expert_a_one_hot += tf.random_normal(tf.shape(expert_a_one_hot), mean=0.2, stddev=0.1, dtype=tf.float32)/1.2 #This can cause problems in my environment so should be careful
+            expert_s_a = tf.concat([self.expert_s, expert_a_one_hot], axis=1) #making state action pairs 
+
+            self.agent_s = tf.placeholder(dtype=tf.float32, shape=[None] + list(env.observation_space.shape))  #agent state 
+            self.agent_a = tf.placeholder(dtype=tf.int32, shape=[None])  
             agent_a_one_hot = tf.one_hot(self.agent_a, depth=env.action_space.n)
+
+
+            '''#If we want to use a LSTM to 
+            self.agent_s = tf.placeholder(tf.float32, [None, max_length, state_dim])
+            self.agent_a = tf.placeholder(tf.float32, [None, max_length, action])
+            agent_a_one_hot = tf.one_hot(self.expert_a, depth=number of actions)
+            '''
             # add noise for stabilise training
             agent_a_one_hot += tf.random_normal(tf.shape(agent_a_one_hot), mean=0.2, stddev=0.1, dtype=tf.float32)/1.2
-            agent_s_a = tf.concat([self.agent_s, agent_a_one_hot], axis=1)
+            agent_s_a = tf.concat([self.agent_s, agent_a_one_hot], axis=1)  #concat agents state action pairs 
 
             with tf.variable_scope('network') as network_scope:
-                prob_1 = self.construct_network(input=expert_s_a)
-                network_scope.reuse_variables()  # share parameter
-                prob_2 = self.construct_network(input=agent_s_a)
+                prob_1 = self.construct_network(input=expert_s_a)  #construct discriminator - You need to put geenrated data well as expert data in to the discriminator 
+                network_scope.reuse_variables()  # share parameter  #Use same parameters very useful
+                prob_2 = self.construct_network(input=agent_s_a) #output probability for the agent
+
+
+            ''' #This is a  lstm discrmonator    
+            with tf.variable_scope('network') as network_scope:
+                prob_1 = self.construct_network(input=expert_s_a)  #construct discriminator - You need to put geenrated data well as expert data in to the discriminator 
+                network_scope.reuse_variables()  # share parameter  #Use same parameters very useful
+                prob_2 = self.construct_network(input=agent_s_a) #output probability for the agent
+            '''
 
             with tf.variable_scope('loss'):
-                loss_expert = tf.reduce_mean(tf.log(tf.clip_by_value(prob_1, 0.01, 1)))
-                loss_agent = tf.reduce_mean(tf.log(tf.clip_by_value(1 - prob_2, 0.01, 1)))
+                loss_expert = tf.reduce_mean(tf.log(tf.clip_by_value(prob_1, 0.01, 1))) #this is like the batch loss
+                loss_agent = tf.reduce_mean(tf.log(tf.clip_by_value(1 - prob_2, 0.01, 1))) #SAme
                 loss = loss_expert + loss_agent
-                loss = -loss
+                loss = -loss  #loss need to be minimize 
                 tf.summary.scalar('discriminator', loss)
 
             optimizer = tf.train.AdamOptimizer()
-            self.train_op = optimizer.minimize(loss)
+            self.train_op = optimizer.minimize(loss) #train op
+            self.rewards = tf.log(tf.clip_by_value(prob_2, 1e-10, 1))  # log(P(expert|s,a)) larger is better for agent #reward is directly calculating for 
+            
 
-            self.rewards = tf.log(tf.clip_by_value(prob_2, 1e-10, 1))  # log(P(expert|s,a)) larger is better for agent
-
-    def construct_network(self, input):
+    def construct_network(self, input): # Discriminator network  This can be a convolutional neural network 
         layer_1 = tf.layers.dense(inputs=input, units=20, activation=tf.nn.leaky_relu, name='layer1')
         layer_2 = tf.layers.dense(inputs=layer_1, units=20, activation=tf.nn.leaky_relu, name='layer2')
         layer_3 = tf.layers.dense(inputs=layer_2, units=20, activation=tf.nn.leaky_relu, name='layer3')
@@ -50,10 +72,12 @@ class Discriminator:
         return prob
 
     def train(self, expert_s, expert_a, agent_s, agent_a):
+
         return tf.get_default_session().run(self.train_op, feed_dict={self.expert_s: expert_s,
                                                                       self.expert_a: expert_a,
                                                                       self.agent_s: agent_s,
                                                                       self.agent_a: agent_a})
+
 
     def get_rewards(self, agent_s, agent_a):
         return tf.get_default_session().run(self.rewards, feed_dict={self.agent_s: agent_s,
--- network_models/policy_net.py
+++ network_models/policy_net.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import pdb
 
 
 class Policy_net:
@@ -9,30 +10,68 @@ class Policy_net:
         :param temp: temperature of boltzmann distribution
         """
 
-        ob_space = env.observation_space
-        act_space = env.action_space
+        ob_space = env.observation_space #this is what explain the observation  - [position of cart, velocity of cart, angle of pole, rotation rate of pole] 
+        act_space = env.action_space  #This is number of actions which is two
+
+        ''' #My OB  and Act spaces
+        ob_space = [vector with 10 elements]
+        act_space=[10 discrete actions] 
+
+        '''
+
+
 
         with tf.variable_scope(name):
             self.obs = tf.placeholder(dtype=tf.float32, shape=[None] + list(ob_space.shape), name='obs')
+            ''' #for the recurrent neural network
+            self.obs = tf.placeholder(tf.float32, [None, max_length, frame_size])   #we do not need to define the maximum time steps since we use dynamic RNN
+            '''
+           
+            def length(sequence):
+                used = tf.sign(tf.reduce_max(tf.abs(sequence), reduction_indices=2))
+                length = tf.reduce_sum(used, reduction_indices=1)
+                length = tf.cast(length, tf.int32)
+                return length
 
-            with tf.variable_scope('policy_net'):
-                layer_1 = tf.layers.dense(inputs=self.obs, units=20, activation=tf.tanh)
+            with tf.variable_scope('policy_net'):  #a small neural network to build policy network 
+                layer_1 = tf.layers.dense(inputs=self.obs, units=20, activation=tf.tanh) #take observations as inputs
                 layer_2 = tf.layers.dense(inputs=layer_1, units=20, activation=tf.tanh)
                 layer_3 = tf.layers.dense(inputs=layer_2, units=act_space.n, activation=tf.tanh)
                 self.act_probs = tf.layers.dense(inputs=tf.divide(layer_3, temp), units=act_space.n, activation=tf.nn.softmax)
+            
+            '''#this is for the dynamic rnn network for my model 
+            with tf.variable_scope('policy_net'):  #a small neural network to build policy network 
+                gru_cell1 = tf.contrib.rnn.GRUCell(128)
+                if self.train:
+                    gru_cell1 = tf.contrib.rnn.DropoutWrapper(gru_cell1, output_keep_prob=0.5)
+                init_state = cell.zero_state(BATCH_SIZE, tf.float32)
+                cell_output, final_stateTuple = tf.nn.dynamic_rnn(gru_cell1,  self.obs, sequence_length=length(self.obs), initial_state=init_state, time_major=False)
+                self.act_probs = tf.layers.dense(inputs=tf.divide(final_stateTuple, temp), units=10, activation=tf.nn.softmax)
+            '''
 
-            with tf.variable_scope('value_net'):
+            with tf.variable_scope('value_net'): #this is can be more like an actor critic but not that much sure -- 
                 layer_1 = tf.layers.dense(inputs=self.obs, units=20, activation=tf.tanh)
                 layer_2 = tf.layers.dense(inputs=layer_1, units=20, activation=tf.tanh)
-                self.v_preds = tf.layers.dense(inputs=layer_2, units=1, activation=None)
+                self.v_preds = tf.layers.dense(inputs=layer_2, units=1, activation=None)  #value prediction for this state 
 
-            self.act_stochastic = tf.multinomial(tf.log(self.act_probs), num_samples=1)
+            '''
+            with tf.variable_scope('value_net'): 
+                gru_cell1 = tf.contrib.rnn.GRUCell(128)
+                if self.train:
+                    gru_cell1 = tf.contrib.rnn.DropoutWrapper(gru_cell1, output_keep_prob=0.5)
+                init_state = cell.zero_state(BATCH_SIZE, tf.float32)
+                cell_output, final_stateTuple = tf.nn.dynamic_rnn(gru_cell1, self.obs, sequence_length=length(self.obs), initial_state=init_state, time_major=False)
+                self.v_preds = tf.layers.dense(inputs=final_stateTuple, units=1, activation=None)  #value prediction for this state 
+
+            '''
+
+            self.act_stochastic = tf.multinomial(tf.log(self.act_probs), num_samples=1) #this is just to act randomly
             self.act_stochastic = tf.reshape(self.act_stochastic, shape=[-1])
+                      
 
-            self.act_deterministic = tf.argmax(self.act_probs, axis=1)
-
+            self.act_deterministic = tf.argmax(self.act_probs, axis=1) #action with maximum pr
             self.scope = tf.get_variable_scope().name
-
+            
     def act(self, obs, stochastic=True):
         if stochastic:
             return tf.get_default_session().run([self.act_stochastic, self.v_preds], feed_dict={self.obs: obs})
